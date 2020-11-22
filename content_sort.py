851649316214d2ca155be16877f6fab87d260898
AustinDevsNumberOne <39441898+AustinDevsNumberOne@users.noreply.github.com>
2020-11-22 11:31:56 +0200

    Update and rename sort.py to content_sort.py

--- content_sort.py
+++ content_sort.py
@@ -2,28 +2,28 @@ import os
 
 import asyncio
 import mimetypes
-
+import math
 from nudenet import NudeClassifier
+from sms_settings import *
 
-
-PATH_TO_SAVE = "./output"
+already_checked = []
 
 
 async def move_nsfw(file):
     try:
         os.rename(file, f"{PATH_TO_SAVE}/nsfw/{file.rsplit('/')[-1]}")
     except FileExistsError:
-        ...
+        print(f"File {file} already in nswf")
 
 
-async def sort():
-
-    loop = asyncio.get_event_loop()
-    detector = await loop.run_in_executor(None, NudeClassifier)
-
-    for file in os.listdir(PATH_TO_SAVE):
+async def sort_algorithm(files, loop, detector, is_already_checked):
+    global already_checked
+    for file in files:
         file = f"{PATH_TO_SAVE}/{file}"
+        if file in already_checked and is_already_checked:
+            continue
 
+        is_already_checked and already_checked.append(file)
         if os.path.isfile(file) and not os.path.isdir(file):
             mime_type = mimetypes.guess_type(file)[0].split("/")[0]
             try:
@@ -41,13 +41,26 @@ async def sort():
                     result = await loop.run_in_executor(None, detector.classify, file)
                     score = result.get(file, {}).get("unsafe", 0)
 
-                if score and score >= 0.8:
+                if score and score >= NSFW_SENSITIVE:
                     await move_nsfw(file)
             except LookupError:
-                ...
+                print(f"Can`t identify image/video {file} content by neuron network.")
+
+
+async def sort_function(is_already_checked=True):
+
+    loop = asyncio.get_event_loop()
+    detector = await loop.run_in_executor(None, NudeClassifier)
+
+    all_files = os.listdir(PATH_TO_SAVE)
+    all_files_len = len(all_files)
+    middle_index = math.ceil(all_files_len/2)
+    await sort_algorithm(all_files[:middle_index], loop, detector, is_already_checked)
+    await sort_algorithm(all_files[middle_index:], loop, detector, is_already_checked)
+
 try:
     os.makedirs(f"{PATH_TO_SAVE}/nsfw")
 except FileExistsError:
-    ...
+    print("Dirs already exists")
 
-asyncio.run(sort())
+asyncio.run(sort_function(is_already_checked=False))
